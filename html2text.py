9a68e8c7ed562cc6e9e0f234e905dc122915ed53
Jonathan Feinberg <feinberg@google.com>
2010-07-08 15:51:37 -0400

    Remove broken import and cygwin-specific hack

--- html2text.py
+++ html2text.py
@@ -3,26 +3,26 @@ from subprocess import PIPE, Popen
 import re
 
 """
-To the extent possible under law, Jonathan Feinberg  has waived all copyright 
-and related or neighboring rights to html2text. This work is published from United States. 
+To the extent possible under law, Jonathan Feinberg  has waived all copyright
+and related or neighboring rights to html2text. This work is published from United States.
 """
 
 class TextFormatter:
     def __init__(self, process=None, lynx='/usr/bin/lynx'):
         self.lynx = lynx
         self.process = process if process else lambda t:t
-    
+
     def html2text(self, unicode_html_source):
         "Expects unicode; returns unicode"
-        text = Popen([self.lynx, 
-                      '-assume-charset=UTF-8', 
-                      '-display-charset=UTF-8', 
-                      '-dump', 
-                      '-stdin'], 
-                      stdin=PIPE, 
+        text = Popen([self.lynx,
+                      '-assume-charset=UTF-8',
+                      '-display-charset=UTF-8',
+                      '-dump',
+                      '-stdin'],
+                      stdin=PIPE,
                       stdout=PIPE).communicate(input=unicode_html_source.encode('utf-8'))[0].decode('utf-8')
         return self.process(text)
-    
+
     def htmlfile2text(self, path, encoding='utf-8'):
         "Returns unicode. Attempts to decode bytes in given file as utf-8 by default."
         with open(path, "r") as f:
@@ -37,7 +37,4 @@ if __name__ == "__main__":
         text = sys.stdin.read()
     encoding = sys.argv[2] if len(sys.argv) > 2 else 'utf-8'
     unicode_html = text.decode(encoding)
-    
-    import converter
-    print TextFormatter(lynx="c:/cygwin/bin/lynx.exe").html2text(unicode_html).encode('utf-8')
-
+    print TextFormatter().html2text(unicode_html).encode('utf-8')
